ef21ac1b45184e9b741b0b3c331f3172131b870c
nithin7316 <nithinkumar6330.nk@gmail.com>
2024-01-31 22:42:34 +0530

    day4

---
+++ day4.js
@@ -0,0 +1,68 @@
+//q1
+// let age2 = prompt("Enter ur age");
+// if (age2 >= 18) {
+//   console.log("You are old enough to drive");
+// } else {
+//   console.log(`ur left with ${18 - age2} year to drive`);
+// }
+
+// let age1 = prompt("enter ur age");
+// let nav = 18 - age1;
+// if (age1 >= 18) {
+//   console.log("You are old enough to drive");
+// } else if (nav) {
+//   console.log("Ur left with " + nav + " years to drive");
+// }
+
+//q2
+// let myage = prompt("Enter my age");
+// let urage = prompt("Enter ur age");
+
+// let ma = myage - urage; //-2
+// let ua = urage - myage; //2
+
+// if (myage === urage) {
+//   console.log("both same");
+// } else if (myage > urage) {
+//   console.log(`I'm ${ma} year older then u`);
+// } else {
+//   console.log(`your ${ua} year older then me`);
+// }
+
+// if (myage > urage) {
+//   console.log(`I'm ${ma}year older then u`);
+// } else {
+//   console.log(`your ${ua}year older then me`);
+// }
+
+//q4
+// let a = prompt("enter the value of a");
+// let b = prompt("enter the value of b");
+
+// if (a > b) {
+//   console.log(`${a} is greater than ${b}`);
+// } else {
+//   console.log(`${a} is less than ${b}`);
+// }
+
+// {
+//   a > b
+//     ? console.log(`${a} is greater than ${b}`)
+//     : console.log(`${a} is less than ${b}`);
+// }
+
+// let score = prompt("Enter the marks");
+
+// if (score >= 80 && score <= 100) {
+//   console.log("A");
+// } else if (score >= 70 && score <= 89) {
+//   console.log("B");
+// } else if (score >= 60 && score <= 69) {
+//   console.log("C");
+// } else if (score >= 50 && score <= 59) {
+//   console.log("D");
+// } else if (score >= 0 && score <= 49) {
+//   console.log("F");
+// } else {
+//   console.log("Invalid marks");
+// }
