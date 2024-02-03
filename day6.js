139956802df6a0935d2585479ed58f8063df7ca2
nithin7316 <nithinkumar6330.nk@gmail.com>
2024-02-03 22:36:11 +0530

    .

---
+++ day6.js
@@ -0,0 +1,49 @@
+//q1
+// for (a = 0; a <= 10; a++) {
+//   console.log(a);
+// }
+
+// //q2
+// for (a = 10; a >= 0; a--) {
+//   console.log(a);
+// }
+
+//q3
+
+// let n = 5;
+// for (a = 1; a <= n; a++) {
+//   console.log("hi");
+// }
+
+//q4
+// const numberOfRows = 7;
+
+// for (let i = 1; i <= numberOfRows; i++) {
+//   let row = "";
+
+//   for (let j = 1; j <= i; j++) {
+//     row += "#";
+//   }
+
+//   console.log(row);
+// }
+
+//q5
+// debugger;
+// for (a = 0; a <= 10; a++) {
+//   let b = a * a;
+//   console.log(`${a}x${a} = ${b}`);
+// }
+
+// //q6
+// console.log("i\ti^2\ti^3\ti^4\n---------------");
+// for (a = 0; a <= 10; a++) {
+//   console.log(`${a} ${a ** 2} ${a ** 3} ${a ** 4}`);
+// }
+
+//q7
+for (let a = 0; a <= 100; a++) {
+  if (a % 2 !== 0) {
+    console.log(a);
+  }
+}
