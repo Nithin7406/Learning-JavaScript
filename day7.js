68e8937e5c2540614acf2497ff8766211f4e9c8d
nithin7316 <nithinkumar6330.nk@gmail.com>
2024-02-09 22:24:56 +0530

    day7

---
+++ day7.js
@@ -0,0 +1,52 @@
+// function vowels(str) {
+//   let count = 0;
+//   for (const check of str) {
+//     if (
+//       check === "a" ||
+//       check === "e" ||
+//       check === "i" ||
+//       check === "o" ||
+//       check === "u"
+//     ) {
+//       count++;
+//     }
+//   }
+//   console.log(count);
+// }
+
+// //
+
+// let a = [0925498712694786, 12376493749, 872938749123];
+// a.forEach((b) => {
+//   console.log(b + b);
+// });
+
+//
+// let a = [90, 96, 80, 92, 88, 86];
+// let newarr = a.filter((val) => {
+//   return val >= 90;
+// });
+// console.log(newarr);
+
+// debugger;
+// let n = prompt("enter the number");
+// let ar = [];
+// for (let a = 1; a <= n; a++) {
+//   ar[a - 1] = a;
+// }
+// console.log(ar);
+
+// let sum = ar.reduce((i, j) => {
+//   return i + j;
+// });
+// console.log(sum);
+
+// let mul = ar.reduce((i, j) => {
+//   return i * j;
+// });
+// console.log(mul);
+
+const arr = [
+  { id: 1, name: "naveen" },
+  { id: 2, name: "nithin" },
+];
