6c7c6814af9c0c151f7832dfb7876c6054b4e978
nithin7316 <nithinkumar6330.nk@gmail.com>
2024-02-16 00:44:17 +0530

    project day2

--- projects/day1.js
+++ projects/day1.js
@@ -1,21 +1,21 @@
 const button = document.querySelector("button");
-button.addEventListener("click", (event) => {
-  console.log("button clicked");
-});
+// button.addEventListener("click", (event) => {
+//   console.log("button clicked");
+// });
 
-//
+// //
 
-// const clickMe = (event) => {
-//   debugger;
-//   alert("We can attach event on HTML element");
-// };
+// // const clickMe = (event) => {
+// //   debugger;
+// //   alert("We can attach event on HTML element");
+// // };
 
-const input = document.querySelector("input");
-input.addEventListener("blur", (e) => {
-  //   console.log(e.target);
-  console.log(e.target.value);
-});
+// const input = document.querySelector("input");
+// input.addEventListener("blur", (e) => {
+//   //   console.log(e.target);
+//   console.log(e.target.value);
+// });
 
-document.addEventListener("keypress", (e) => {
-  console.log(e.key);
-});
+// document.addEventListener("keypress", (e) => {
+//   console.log(e.key);
+// });
--- projects/script.js
+++ projects/script.js
@@ -1,3 +1,111 @@
+// function isPrime(num) {
+//   if (num <= 1) return false;
+//   if (num <= 3) return true;
+//   if (num % 2 === 0 || num % 3 === 0) return false;
+//   for (let i = 5; i * i <= num; i += 6) {
+//     if (num % i === 0 || num % (i + 2) === 0) return false;
+//   }
+//   return true;
+// }
+
+// ///
+// const container = document.getElementById("container");
+
+// const input = document.querySelector("input");
+// input.addEventListener("blur", () => {
+//   const N = 100;
+
+//   const numRows = Math.ceil(N / 10);
+//   for (let i = 0; i < numRows; i++) {
+//     const row = document.createElement("div");
+//     row.classList.add("row");
+
+//     for (let j = 1; j <= 10; j++) {
+//       const number = i * 10 + j;
+//       if (number > N) break;
+//       const div = document.createElement("div");
+//       div.textContent = number;
+//       div.classList.add("number");
+
+//       div.style.display = "inline-block";
+//       div.style.width = "50px";
+//       div.style.height = "50px";
+//       div.style.textAlign = "center";
+//       div.style.lineHeight = "50px";
+//       div.style.margin = "2px";
+
+//       if (number % 2 === 0) {
+//         div.style.backgroundColor = "lightgreen";
+//       } else {
+//         div.style.backgroundColor = "yellow";
+//       }
+
+//       if (isPrime(number)) {
+//         div.style.backgroundColor = "red";
+//       }
+
+//       row.appendChild(div);
+//     }
+
+//     container.appendChild(row);
+//   }
+// });
+
+const container = document.getElementById("container");
+const input = document.querySelector("input");
+
+input.addEventListener("blur", () => {
+  // Get the value entered in the input field
+  const inputValue = parseInt(input.value, 10);
+
+  // Check if the entered value is valid (a positive integer)
+  if (Number.isInteger(inputValue) && inputValue > 0) {
+    // Clear the container before generating new numbers
+    container.innerHTML = "";
+
+    const N = inputValue;
+
+    const numRows = Math.ceil(N / 10);
+    for (let i = 0; i < numRows; i++) {
+      const row = document.createElement("div");
+      row.classList.add("row");
+
+      for (let j = 1; j <= 10; j++) {
+        const number = i * 10 + j;
+        if (number > N) break;
+        const div = document.createElement("div");
+        div.textContent = number;
+        div.classList.add("number");
+
+        div.style.display = "inline-block";
+        div.style.width = "50px";
+        div.style.height = "50px";
+        div.style.textAlign = "center";
+        div.style.lineHeight = "50px";
+        div.style.margin = "2px";
+
+        if (number % 2 === 0) {
+          div.style.backgroundColor = "lightgreen";
+        } else {
+          div.style.backgroundColor = "yellow";
+        }
+
+        if (isPrime(number)) {
+          div.style.backgroundColor = "red";
+        }
+
+        row.appendChild(div);
+      }
+
+      container.appendChild(row);
+    }
+  } else {
+    // If the entered value is not valid, display an error message or handle it accordingly
+    console.log("Please enter a valid positive integer.");
+  }
+});
+
+// Function to check if a number is prime
 function isPrime(num) {
   if (num <= 1) return false;
   if (num <= 3) return true;
@@ -7,55 +115,3 @@ function isPrime(num) {
   }
   return true;
 }
-
-///
-const container = document.getElementById("container");
-
-const N = 100;
-
-const numRows = Math.ceil(N / 5);
-for (let i = 0; i < numRows; i++) {
-  const row = document.createElement("div");
-  row.classList.add("row");
-
-  for (let j = 1; j <= 5; j++) {
-    const number = i * 5 + j;
-    if (number > N) break;
-    const div = document.createElement("div");
-    div.textContent = number;
-    div.classList.add("number");
-
-    div.style.display = "inline-block";
-    div.style.width = "50px";
-    div.style.height = "50px";
-    div.style.textAlign = "center";
-    div.style.lineHeight = "50px";
-    div.style.margin = "2px";
-
-    if (number % 2 === 0) {
-      div.style.backgroundColor = "lightgreen";
-    } else {
-      div.style.backgroundColor = "yellow";
-    }
-
-    if (isPrime(number)) {
-      div.style.backgroundColor = "red";
-    }
-
-    row.appendChild(div);
-  }
-
-  container.appendChild(row);
-}
-
-const countriesAPI = "https://restcountries.com/v2/all";
-
-fetch(countriesAPI)
-  .then((response) => response.json())
-  .then((item) => {
-    item.forEach((country) => {
-      const name = country.name;
-      console.log(name);
-    });
-  })
-  .catch((error) => console.error("Error fetching data:", error));
